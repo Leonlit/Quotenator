a160d615eb5a7d7c1848f07093f9da54466eed33
Leonlit <leonlit123@gmail.com>
2019-09-22 16:13:40 +0800

    cleared some console.log method

--- script/index.js
+++ script/index.js
@@ -16,7 +16,6 @@ function setlocalstore(setTime) {
 		last_time = localStorage.getItem("usertime");
 		//if user time is null or after the time is 10s forward
 		if ( last_time == null || (militime - last_time) > 5000) {
-			console.log("changed time")
 			//variable
 			let imageNum = 6
 			//need to find ways to take totally random image from somewhere else
@@ -42,7 +41,6 @@ function setlocalstore(setTime) {
 					//quotes author and the contents
 					$('#author').text(" ~ " + post.title.rendered);
 					$('#content').html(post.content.rendered);
-					console.log(data);
 				},
 				cache: false
 			});
@@ -62,7 +60,6 @@ function setlocalstore(setTime) {
 
 /* Set the width of the side navigation to 45% & shader opacity to 0.6 */
 function openNav() {
-	console.log(innerWidth);
 	if (window.innerWidth >= 800) {
 		$("#sideNav").css("width","40%");
 		$(".shader").css("opacity","0.6");
@@ -74,7 +71,6 @@ function openNav() {
 
 /* Set the width of the side navigation to 0 & shader opacity to 0*/
 function closeNav() {
-	console.log(window.innerWidth);
 	if (window.innerWidth >= 800) {
 		$("#sideNav").css("width","0");
 		$(".shader").css("opacity","0");
