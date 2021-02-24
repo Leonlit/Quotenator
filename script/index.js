810c90aa6b4940f898dfb6fa197dbb7b5b662c7d
Leon <leonlit123@gmail.com>
2021-02-24 23:13:38 +0800

    fixed close button, positioning, and structure

--- script/index.js
+++ script/index.js
@@ -21,16 +21,12 @@ $(window).on('load', function(e) {
 					let post = data.shift();
 					//quotes author and the contents
 					$("#content").removeClass("fadeIn");
-					$("#author").removeClass("fadeIn");
 					$("#content").addClass("fadeOut");
-					$("#author").addClass("fadeOut");
 					setTimeout(()=>{
 						$('#author').text(" ~ " + post.title.rendered);
-						$('#content').html(post.content.rendered);
+						$('#body').html(post.content.rendered);
 						$("#content").removeClass("fadeOut");
-						$("#author").removeClass("fadeOut");
 						$("#content").addClass("fadeIn");
-						$("#author").addClass("fadeIn");
 					}, 500);
 				},
 				cache: false
